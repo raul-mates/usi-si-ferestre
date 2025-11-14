9b1a63cc56005f0422d91bf48934fe2c012e6da5
Mates Raul <matesraulalexandru@gmail.com>
2025-11-14 18:02:15 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {Homepage} from './pages/homepage/homepage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'homepage', pathMatch: 'full' },
+  { path: 'homepage', component: Homepage },
+];
