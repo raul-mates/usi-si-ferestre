f5007be5242222edecc7a01d8792137e8da6ffa4
Mates Raul <matesraulalexandru@gmail.com>
2025-11-16 14:34:15 +0100

    improved responsive design

---
+++ src/app/pages/under-construction/under-construction.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-under-construction',
+  imports: [],
+  templateUrl: './under-construction.html',
+  styleUrl: './under-construction.scss',
+})
+export class UnderConstruction {
+
+}
