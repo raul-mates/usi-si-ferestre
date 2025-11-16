a4406cc8f3eff9e82d5916747ed83e9ac51367dd
Mates Raul <matesraulalexandru@gmail.com>
2025-11-16 22:39:04 +0100

    various modifications, added footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NavBar} from './shared/components/nav-bar/nav-bar';
 import {ContactInfoBar} from './shared/components/contact-info/contact-info-bar';
+import {Footer} from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar, ContactInfoBar],
+  imports: [RouterOutlet, NavBar, ContactInfoBar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   standalone: true
