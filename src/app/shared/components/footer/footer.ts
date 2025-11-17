6e277e2ee8c595bb2c3d82c5d3846c400a20dd0b
Mates Raul <matesraulalexandru@gmail.com>
2025-11-17 23:58:10 +0100

    added most of the footer content

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +7,5 @@ import {Component} from '@angular/core';
   styleUrl: './footer.scss',
 })
 export class Footer {
-
+  currentYear = signal(new Date().getFullYear())
 }
